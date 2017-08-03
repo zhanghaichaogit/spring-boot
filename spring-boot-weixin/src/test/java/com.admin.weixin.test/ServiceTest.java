706b5afe181117bf7d73bd340563697eb2f9e147
zhanghaichao <704877313@qq.com>
2017-08-03 14:50:50 +0800

    提交测试类

---
+++ spring-boot-weixin/src/test/java/com.admin.weixin.test/ServiceTest.java
@@ -0,0 +1,17 @@
+package com.admin.weixin.test;
+
+import com.admin.weixin.SimpleApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author zhanghaichao on 2017/8/3.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SimpleApplication.class)
+public class ServiceTest {
+
+
+
+}
